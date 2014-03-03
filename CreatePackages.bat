ebc8aa236a329f9e8ac8a1fa85a7496eaa76ccc2
jrote1 <jrote1@me.com>
2014-03-03 17:53:39 +0000

    Added MVC5 Build

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,10 +1,12 @@
 @echo off
 "%systemroot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "PseudoCQRS\PseudoCQRS.csproj" /v:normal /t:Rebuild /p:Configuration=Mvc3
 "%systemroot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "PseudoCQRS\PseudoCQRS.csproj" /v:normal /t:Rebuild /p:Configuration=Mvc4
+"%systemroot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "PseudoCQRS\PseudoCQRS.csproj" /v:normal /t:Rebuild /p:Configuration=Mvc51
 
 if ERRORLEVEL 1 goto eof
 
 nuget pack PseudoCQRS.nuspec
 nuget pack PseudoCQRS.Mvc4.nuspec
+nuget pack PseudoCQRS.Mvc5.1.nuspec
 
 :eof
