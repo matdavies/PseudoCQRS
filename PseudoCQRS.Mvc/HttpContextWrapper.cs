e3356ef3bb9a45a4538facf34e0a106426d27015
Adeel <adeel41@yahoo.com>
2014-10-23 23:26:52 +0100

    added a mvc project which will contain all mvc related files

---
+++ PseudoCQRS.Mvc/HttpContextWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PseudoCQRS.Mvc
+{
+    public class HttpContextWrapper : IHttpContextWrapper
+    {
+    }
+}
